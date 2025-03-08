6e2e44252a189cd3d23de02c397b2ce445f1e15d
Mayar Ahmed <myar.ahmed818@gmail.com>
2025-03-08 21:12:40 +0200

    Login Dropdown of Header

--- Ta7tBety/src/Components/Header.jsx
+++ Ta7tBety/src/Components/Header.jsx
@@ -1,45 +1,98 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 
-function Header(){
-return(
+function Header() {
+  return (
     <>
-    <div className="container-fluid HeaderBorderBottom">
-    <div className="Ta7tBety-Header">
-    <span className="lobster-regular">Ta7t Bety</span>
-    <div className="header-Elements">
-        <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/AboutUS">About Us</Link></li>
-            <li><Link to="">Services</Link></li>
-            <li><Link to="/HelpCenter">Help Center</Link></li>
-            <li><Link to="/Signin">Sign In</Link></li>
-            <li><Link to="/Signup">Sign Up</Link></li>
+      <div className="container-fluid HeaderBorderBottom">
+        <div className="Ta7tBety-Header">
+          <span className="lobster-regular">Ta7t Bety</span>
+          <div className="header-Elements">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/AboutUS">About Us</Link>
+                </li>
+                <li>
+                  <Link to="">Services</Link>
+                </li>
+                <li>
+                  <Link to="/HelpCenter">Help Center</Link>
+                </li>
+                {/* <li><Link to="/Signin">Sign In</Link></li> */}
+                {/* <li><Link to="/Signup">Sign Up</Link></li> */}
+              </ul>
+            </nav>
+          </div>
 
-            {/* <li><Link to={}></Link></li>
-            <li><Link to={}></Link></li>
-            <li><Link to={}></Link></li> */}
+          <div className="header-icons">
+            <ul>
+              <li>
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </li>
+              <li>
+                <i class="fa-regular fa-heart"></i>
+              </li>
+              <li>
+                <Link to="/Profile">
+                  <i class="fa-regular fa-user"></i>
+                </Link>
+              </li>
 
-            {/* <li><a href="">Home</a></li>
-            <li><a href="" >About Us</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Help Center</a></li> */}
-        </ul>
-        </nav>
-    </div>
+              <li>
+                <Dropdown>
+                  <Dropdown.Toggle className="btn-light">
+                    Login
+                  </Dropdown.Toggle>
 
-    <div className="header-icons">
-        <ul>
-            <li><i class="fa-solid fa-magnifying-glass"></i></li>
-            <li><i class="fa-regular fa-heart"></i></li>
-            <li><Link to="/Profile"><i class="fa-regular fa-user"></i></Link></li>
-        </ul>
-    </div>
-    </div>
-    </div>
+                  <Dropdown.Menu className="DropDownMenue">
+                    <Dropdown.Item className="SignOption" href="#/action-1"><Link to="/Signin">Sign In</Link></Dropdown.Item>
+                    <Dropdown.Item className="SignupOption" href="#/action-2">
+                       <Link to="/Signup">Not a Memeber? <span>Sign Up</span></Link>
+
+                    </Dropdown.Item>
+                
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+
+
+
+
+              {/* <div className="dropdown">
+                <button
+                  className="btn loginHeaderbtn dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Log In
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <button className="dropdown-item dropdownSignin">
+                      Sign In
+                    </button>
+                  </li>
+                  <li>
+                    <button className="dropdown-item dropdownSignUp">
+                      <p>Not a Member? Sign Up</p>
+                    </button>
+                  </li>
+                </ul>
+              </div> */}
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-)
+  );
 }
 
 export default Header;
